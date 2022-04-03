f88884a18615cc608bfc11742ad27c5f4ac2c890
priyanka <priyanka.chavan1408@gmail.com>
2022-04-03 19:23:19 +0530

    [Priyanka]Added:Welcome message on the master branch.

---
+++ EmployeeWageBuilder.py
@@ -0,0 +1,9 @@
+'''
+@Author : Priyanka
+@Date : 2022-05-02  19:20:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-05-02  19:25:00
+@Title : Printing the welcome message.
+'''
+
+print("Welcome to the employee wage computation program")
