4410888a0363ab3009ce120aa0c5411aab0b21a8
priyanka <priyanka.chavan1408@gmail.com>
2022-04-03 19:41:29 +0530

    [Priyanka]Added:Calculated daily employee wage.

--- EmployeeWageBuilder.py
+++ EmployeeWageBuilder.py
@@ -1,9 +1,9 @@
 '''
 @Author : Priyanka
-@Date : 2022-05-02  19:25:00
+@Date : 2022-05-02  19:38:00
 @Last Modified by : Priyanka
-@Last Modified Time : 2022-05-02  19:35:00
-@Title : Checking employee is present or not.
+@Last Modified Time : 2022-05-02  19:44:00
+@Title : Calculating daily employee wage.
 '''
 
 import random
@@ -11,9 +11,16 @@ import random
 print("Welcome to the employee wage computation program.")
 
 IS_Full_Time = 1
+EMP_RATE_PER_HOUR = 20
+empHours = 0
+empWage = 0
 empCheck = random.randint(0,1)
 
 if empCheck == IS_Full_Time:
     print("Employee is present")
+    empHours = 8
 else:
     print("Employee is absent")
+    empHours = 0
+empWage = empHours * EMP_RATE_PER_HOUR
+print("Employee wage is",empWage)
